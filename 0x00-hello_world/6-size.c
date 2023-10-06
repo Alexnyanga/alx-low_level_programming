5820b9f669da6863a7f20f788d73d623f48e14dc
Alexnyanga <obwogialex26@gmail.com>
2023-10-06 03:35:20 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
-	/**
-	 * *main – A program to print the size of types
-	 * *Return: (0) 
-	 * */
-	int main(void)
-	{
-		char a;
-		int ;
-		long int c;
-		long long int d;
-		float e;
-		printf(“The size of a char is: %lu byte(s)\n”, (unsigned long)sizeof(a));
-		printf(“The size of an int is: %lu byte(s)\n”, (unsigned long)sizeof(b));
-		printf(“The size of a long int is: %lu byte(s)\n”, (unsigned long)sizeof(c));
-		printf(“The size of a long long int is: %lu byte(s)\n”, (unsigned long)sizeof(d)); 
-		printf(“The size of a float is: %lu byte(s)\n”, (unsigned long)sizeof(e));
-		return 0;
-	}
+/**
+ *main – A program to print the size of types
+ *Return: (0) 
+ */
+int main(void)
+{
+        char a;
+	int ;
+	long int c;
+	long long int d;
+	float e;
+printf(“The size of a char is: %lu byte(s)\n”, (unsigned long)sizeof(a));
+printf(“The size of an int is: %lu byte(s)\n”, (unsigned long)sizeof(b));
+printf(“The size of a long int is: %lu byte(s)\n”, (unsigned long)sizeof(c));
+printf(“The size of a long long int is: %lu byte(s)\n”, (unsigned long)sizeof(d)); 
+printf(“The size of a float is: %lu byte(s)\n”, (unsigned long)sizeof(e));
+return (0);
+}
 
