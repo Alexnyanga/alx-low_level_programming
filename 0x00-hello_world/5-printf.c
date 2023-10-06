85081d64e4e8006a1b42a07089ab043af3e05183
Alexnyanga <obwogialex26@gmail.com>
2023-10-05 23:35:58 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main - printing an line with printf
+ * Return: (0)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,/");
+	return (0);
+}
