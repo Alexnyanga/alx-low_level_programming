05fd4ec3750aae696ec7abfe508197242ce94978
Alexnyanga <obwogialex26@gmail.com>
2023-10-08 15:26:45 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - printing lowercase numbers
+ * Return:m(0)
+ */
+
+int main(void)
+
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low &&!q, &&!e);
+
+	for (low = 'A'; low <= 'Z'; low++)
+		putchar(low &&!Q, &&!E);
+
+	return (0);
+
+}
