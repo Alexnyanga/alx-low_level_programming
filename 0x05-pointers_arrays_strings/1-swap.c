00260f582823323faeafd9ad885fe906a5bb59f8
Alexnyanga.com <obwogialex26@gmail.com>
2023-10-18 12:46:22 +0300

    dont swap horses..

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * swap int - swaps the values of two integers.
+ * swap_int - swaps the values of two integers.
  * @a: The first value to be swapped.
  * @b: The second value to be swapped.
  * Return: nothing
